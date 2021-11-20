dd4c1ca484fca6d8a9d891d841973a326b4a7b5f
津守俊輔 <tsumori.shunsuke@gmail.com>
2021-11-20 15:21:00 +0900

    p83 better solution

--- src/main/kotlin/com/example/kotlinpesolutions/solver/p001_100/P083Solver.kt
+++ src/main/kotlin/com/example/kotlinpesolutions/solver/p001_100/P083Solver.kt
@@ -24,77 +24,62 @@ class P083Solver : ProjectEulerSolver {
     }
 
     private fun dijkstra(): Int {
-        val dNodes = mutableListOf<Pair<Int, Int>>() // destinations
+        val nodes = mutableListOf<Node>()
         for (i in 0 until HEIGHT) {
             for (j in 0 until WIDTH) {
-                dNodes.add(Pair(i, j))
+                nodes.add(Node(i, j, Int.MAX_VALUE))
             }
         }
-        dNodes.remove(Pair(0, 0))
-        val oNodes = mutableListOf(Pair(0, 0)) // origins
-        val costMap = mutableMapOf(Pair(0, 0) to MATRIX[0][0])
+        val startNode = nodes[0]
+        startNode.minCost = MATRIX[0][0]
+        val queue = PriorityQueue<Node>()
+        queue.add(startNode)
 
-        while (dNodes.contains(Pair(HEIGHT - 1, WIDTH - 1))) {
-            val (nextNode, theCost) = determineNextNodeAndTheCost(oNodes, dNodes, costMap)
-            dNodes.remove(nextNode)
-            oNodes.add(nextNode)
-            costMap[nextNode] = theCost
-            println(oNodes.size)
+        while (queue.isNotEmpty()) {
+            val current = queue.poll()
+            val currentX = current.x
+            val currentY = current.y
+            calculateFor(currentX + 1, currentY, current, nodes, queue)
+            calculateFor(currentX - 1, currentY, current, nodes, queue)
+            calculateFor(currentX, currentY + 1, current, nodes, queue)
+            calculateFor(currentX, currentY - 1, current, nodes, queue)
         }
-        return costMap[Pair(HEIGHT - 1, WIDTH - 1)]!!
+
+        return nodes[HEIGHT * WIDTH - 1].minCost
     }
 
-
-    private fun determineNextNodeAndTheCost(
-        oNodes: List<Pair<Int, Int>>,
-        dNodes: List<Pair<Int, Int>>,
-        costMap: Map<Pair<Int, Int>, Int>
-    ): Pair<Pair<Int, Int>, Int> {
-        PriorityQueue<Pair<Int, Int>>()
-        var minCost = Int.MAX_VALUE
-        var nextNode = dNodes[0]
-        for (oNode in oNodes) {
-            val (x, y) = oNode
-            if (isExistsPathTo(x + 1, y, oNodes)) {
-                val cost = costMap[oNode]!! + MATRIX[x + 1][y]
-                if (cost < minCost) {
-                    minCost = cost
-                    nextNode = Pair(x + 1, y)
-                }
-            }
-            if (isExistsPathTo(x - 1, y, oNodes)) {
-                val cost = costMap[oNode]!! + MATRIX[x - 1][y]
-                if (cost < minCost) {
-                    minCost = cost
-                    nextNode = Pair(x - 1, y)
-                }
-            }
-            if (isExistsPathTo(x, y + 1, oNodes)) {
-                val cost = costMap[oNode]!! + MATRIX[x][y + 1]
-                if (cost < minCost) {
-                    minCost = cost
-                    nextNode = Pair(x, y + 1)
-                }
-            }
-            if (isExistsPathTo(x, y - 1, oNodes)) {
-                val cost = costMap[oNode]!! + MATRIX[x][y - 1]
-                if (cost < minCost) {
-                    minCost = cost
-                    nextNode = Pair(x, y - 1)
-                }
+    private fun calculateFor(x: Int, y: Int, currentNode: Node, nodes: List<Node>, nodeQueue: PriorityQueue<Node>) {
+        if (isExistsPathTo(x, y)) {
+            val nextNode = nodes.filter { n -> n.x == x && n.y == y }[0]
+            val cost = currentNode.minCost + MATRIX[x][y]
+            if (nextNode.minCost > cost) {
+                nextNode.minCost = cost
+                nodeQueue.add(nextNode)
             }
         }
-        return Pair(nextNode, minCost)
     }
 
-    private fun isExistsPathTo(i: Int, j: Int, oNodes: List<Pair<Int, Int>>): Boolean {
+    private fun isExistsPathTo(i: Int, j: Int): Boolean {
         if (i < 0 || HEIGHT <= i || j < 0 || WIDTH <= j) {
             return false
         }
-        if (oNodes.contains(Pair(i, j))) {
-            return false
-        }
         return true
+    }
+
+    private class Node(
+        val x: Int,
+        val y: Int,
+        var minCost: Int,
+    ) : Comparable<Node> {
+        override fun compareTo(other: Node): Int {
+            return if (this.minCost == other.minCost) {
+                0
+            } else if (other.minCost < this.minCost) {
+                1
+            } else {
+                -1
+            }
+        }
     }
 
     companion object {
